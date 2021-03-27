8ca27a687f7ebd4619af57b685b8717da877629c
SteamDwarf <schajckodmitrij@gmail.com>
2021-03-27 15:29:50 +0300

    Nuton_Dichotomy_nearly_finifshed

--- ПР 3. Решение нелинейных уравнений/script.js
+++ ПР 3. Решение нелинейных уравнений/script.js
@@ -1,82 +1,35 @@
 "use strict"
 
 document.addEventListener("DOMContentLoaded", () => {
-
-    let yCordinates = [],
-        xTrueCordinates = [], 
+    const aInput = document.querySelector('[name="a-cordinate"]'),
+          bInput = document.querySelector('[name="b-cordinate"]'),
+          outputBlock = document.querySelector('.output_block'),
+          btnCountNuton = document.querySelector('#Nuton_btn'),
+          btnCountDichotomy = document.querySelector('#Dichotomy_btn'),
+          iterationOutput = document.querySelector('.iteration_output'),
+          resOutput = document.querySelector('.res_output'),
+          errorOutput = document.querySelector('.error_output');
+          
+    let a, b, 
         step = 0.5,
-        x, a = -4, b = -2,
-        xCordinates = [a],
-        expression = '3*x*Math.sin(x)-1',
-        derivativeFirst = '3*Math.sin(x) + 3*x*Math.cos(x)';
-/*         expression2 = '2^x-2*(x^2)+1',
-        expression3 = 'x*2^x-1'; */
-
-
-   /*  function createCordinates() {
-        createNewX();
-        findingA_B();
-        xCordinates = [a];
-        //console.log(xCordinates);
-        createNewX();
-        //console.log(xCordinates);
-        yCordinates = [];
-        xCordinates.forEach(num => {
-            x = num;
-            yCordinates.push(eval(expression)); 
-        });
-        console.log(yCordinates);
-        //console.log(a, b);
-        //console.log(Math.abs((a - b) / step));
-        lineDraw();
-    }
-
-    function createNewX() {
-        if(!a && !b) {
-            for(let i = 0; i < 30; i++) {
-                xCordinates.push(xCordinates[i] + step);
-            }
-        }else {
-            for(let i = 0; i < Math.abs((a - b) / step); i++) {
-                xCordinates.push(xCordinates[i] + step);
-            }
-            
-        }
-        
-        //console.log(xCordinates); 
-    }
-
-    function findingA_B() {
-        xCordinates.forEach(num => {
-            x = num;
-            let y = eval(expression); 
-            if(y < 1 && y > -1) {
-                xTrueCordinates.push(num);
-                yCordinates.push(y);
-            }
-           
-        });
-        //console.log(yCordinates);
-        a = (Math.min.apply(null, yCordinates));
-        b = (Math.max.apply(null, yCordinates));
-        //console.log(a, b);
-    }
- */
-
-    function createX() {
-        for(let i = 0; i <Math.abs((b - a) / step); i++) {
-            xCordinates.push(xCordinates[i] + step);
-        }
-        console.log(xCordinates);
-    }
-
-    function createY() {
-        xCordinates.forEach(num => {
-            x = num;
-            yCordinates.push(eval(expression));
-        });
-        console.log(yCordinates);
-    }
+        eps = 0.00001,     
+        xCordinatesChart = [],
+        yCordinatesChart = [],
+        xApproximateCordinates = [],
+        yApproximateCordinates = [],
+        x, y, yDeriv,
+        k = 0,   
+/*         expression4 = '3*x*Math.sin(x)-1',
+        derivativeFirst4 = '3*Math.sin(x) + 3*x*Math.cos(x)'; */
+/*         expression4 = 'x**3+4*x-3', //Это работает
+        derivativeFirst4 = '4*x-7',
+        derivativeSecond4 = '4'; */
+        expression4 = '2*(x**2)-7*x+2',
+        derivativeFirst4 = '4*x-7',
+        derivativeSecond4 = '4';
+/*         expression4 = '2**x-2*(x**2)+1',
+        derivativeFirst4 = '2**x*Math.log(2)-4*x'; */
+       /*  expression3 = 'x*2**x-1'; */
 
     function lineDraw() {
 
@@ -88,11 +41,11 @@ document.addEventListener("DOMContentLoaded", () => {
             
             data.addColumn('number', 'x');
             data.addColumn('number', 'y');
-            data.addRows(yCordinates.length);
+            data.addRows(yCordinatesChart.length);
 
-            for (let i = 0; i < yCordinates.length; i++) {
-                data.setCell(i, 0, xCordinates[i]);
-                data.setCell(i, 1, yCordinates[i]);
+            for (let i = 0; i < yCordinatesChart.length; i++) {
+                data.setCell(i, 0, xCordinatesChart[i]);
+                data.setCell(i, 1, yCordinatesChart[i]);
             }
 
             let options = {
@@ -101,16 +54,144 @@ document.addEventListener("DOMContentLoaded", () => {
             legend: 'none'
             }; 
 
-            let chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+            let chart = new google.visualization.LineChart(document.querySelector('#curve_chart'));
 
             chart.draw(data, options);
         }
     }
+
+    function refresh() {
+        a = ''; 
+        b = '';
+        k = 0;
+        step = 0.5;
+        eps = 0.00001;    
+        xCordinatesChart = [];
+        yCordinatesChart = [];
+        xApproximateCordinates = [];
+        yApproximateCordinates = [];
+        x = null;
+        y = null; 
+        yDeriv = null;   
+    }
+
+    function createChartCordinates() {
+        xCordinatesChart = [a];
+        for(let i = 0; i < Math.abs((a - b) / step); i++) {
+            xCordinatesChart.push(xCordinatesChart[i] + step);
+        }
+        xCordinatesChart.forEach(num => {
+            x = num;
+            let y = eval(expression4);
+            yCordinatesChart.push(y);
+        //console.log(xCordinatesChart); 
+        });
+    }
+
+    function findingApproximate() {
+        yCordinatesChart.forEach((num, i) => {
+            if(num < 1 && num > -1) {
+                xApproximateCordinates.push(xCordinatesChart[i]);
+                yApproximateCordinates.push(yCordinatesChart[i]);
+            }  
+        });
+        if(yApproximateCordinates.length < 1 && step < 0.2) {
+            alert('В этом промежутке нет корней данного уранвения');
+            
+        } else if(yApproximateCordinates.length < 1 && step > 0.2) {
+            yCordinatesChart = [];
+            xApproximateCordinates = [];
+            yApproximateCordinates = [];
+            xCordinatesChart = [-5];
+            step /= 2;
+            createCordinates();
+            findingApproximate();
+        }
+        //console.log(yCordinatesChart);
+        //console.log(a, b);
+        //console.log(xApproximateCordinates);
+        //console.log(yApproximateCordinates);
+    }
     
-    //createCordinates();
-    createX();
-    createY();
-    lineDraw();
+    function createCordinates() {
+        createChartCordinates();
+        lineDraw();
+    }
+
+    function createAproximateCordinates(i) {
+        x = xApproximateCordinates[i];
+        y = yApproximateCordinates[i];
+        yDeriv = eval(derivativeFirst4);
+        console.log(x, y, yDeriv);
+    }
+
+    function iterationNuton() { 
+        do {
+            let newX = x - y / yDeriv;
+            x = newX;
+            y = eval(expression4);
+            yDeriv = eval(derivativeFirst4);
+            k++;
+        } while(Math.abs(y) > eps);
+        //console.log(x, y, yDeriv);
+    }
+
+    function methodNuton() {
+        a = +aInput.value;
+        b = +bInput.value;
+        createCordinates();
+        findingApproximate();
+        xApproximateCordinates.forEach((num, i) => {
+            createAproximateCordinates(i);
+            iterationNuton();
+        });
+    }
+
+
+    function makeSegmentation(yArray) {
+        let c = (a + b) / 2;
+        x = c;
+        y = eval(expression4);
+
+        if(yArray[0] * y > 0) {
+            a = x;
+            yArray[0] = y;
+        } else {
+            b = x;
+            yArray[1] = y;
+        }
+    }
+
+    function methodDichotomy() {
+        a = +aInput.value;
+        b = +bInput.value;
+        createCordinates();
+        let ySegment = [yCordinatesChart[0], yCordinatesChart[yCordinatesChart.length - 1]];
+        
+        do {
+            makeSegmentation(ySegment);
+            k++;
+        }while(Math.abs(y) > eps);
+    }
+
+    btnCountNuton.addEventListener('click', () => {
+        refresh();
+        methodNuton();
+        iterationOutput.textContent = k;
+        resOutput.textContent = x;
+        errorOutput.textContent = y;
+    });
+
+    btnCountDichotomy.addEventListener('click', () => {
+        refresh();
+        methodDichotomy();
+        iterationOutput.textContent = k;
+        resOutput.textContent = x;
+        errorOutput.textContent = y;
+    });
+/*     createX();
+    createY(); */
+    //lineDraw();
 });
 
 
